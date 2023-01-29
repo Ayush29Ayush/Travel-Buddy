468b2feedcdd47760323724ef1698bc193d4fef8
Ayush Senapati <ayushsenapati123@gmail.com>
2023-01-29 19:17:48 +0530

    HomeScreen top done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,9 +12,15 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView className="bg-indigo-300 flex-1 relative">
-      <View>
-        <Text>Hi there</Text>
+    <SafeAreaView className="bg-white flex-1 relative">
+      {/* First Section */}
+      <View className="flex-row px-6 mt-10 items-center space-x-2">
+        <View className="w-24 h-19 bg-black rounded-lg items-center justify-center">
+          <Text className="text-[#00BCC9] text-3xl font-semibold">Travel</Text>
+        </View>
+        <View>
+          <Text className="text-[#2A2B4B] text-3xl fond-semibold">Buddy</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
