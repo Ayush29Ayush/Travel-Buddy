d4729a236618af6107d3e0e7c86888086f6b6d03
Ayush Senapati <ayushsenapati123@gmail.com>
2023-01-29 15:44:12 +0530

    Added SafeAreaView with tailwind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-400">HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-indigo-300 flex-1 relative">
+      <View>
+        <Text>Hi there</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
-
+export default HomeScreen;
