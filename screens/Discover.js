1362e6204c051a1229c60c757e6cb2ba9aab3a25
Ayush Senapati <ayushsenapati123@gmail.com>
2023-02-02 13:28:19 +0530

    Discover screen

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const Discover = () => {
-  return (
-    <View>
-      <Text>Discover</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default Discover
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row items-center justify-between px-8 mt-10">
+        <View>
+          <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527283] text-[30px]">the beauty today</Text>
+        </View>
+
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center">
+          
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Discover;
