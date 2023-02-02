0b323498e6cdbaec869faa45e5587e7fa924fc41
Ayush Senapati <ayushsenapati123@gmail.com>
2023-02-02 20:11:03 +0530

    new avatar added

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
 export {default as HeroImage} from "./hero.png"
 export {default as trialpic} from "./trialpic.png"
 export {default as Avatar} from "./avatar.png"
+export {default as Avatar1} from "./avatar1.jpg"
