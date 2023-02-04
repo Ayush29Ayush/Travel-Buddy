143c090b9defa44e4ea357ecf1fd409b3fa8be9d
Ayush Senapati <ayushsenapati123@gmail.com>
2023-02-04 23:01:21 +0530

    Menu

---
+++ components/MenuContainer.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MenuContainer = () => {
+  return (
+    <View>
+      <Text>MenuContainer</Text>
+    </View>
+  )
+}
+
+export default MenuContainer
