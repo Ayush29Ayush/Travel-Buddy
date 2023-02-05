97e5e698ab7c44247ac297b170354cbdb2d32f93
Ayush Senapati <ayushsenapati123@gmail.com>
2023-02-05 20:51:42 +0530

    3 options done

--- components/MenuContainer.js
+++ components/MenuContainer.js
@@ -1,11 +1,24 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 
-// passing props ro menucontainer
-const MenuContainer = (title, imageSrc, type, setType) => {
+// passing props to MenuContainer
+const MenuContainer = ({ title, imageSrc, type, setType }) => {
+  const handlePress = () => {
+    setType(title.toLowerCase());
+  };
   return (
-    <TouchableOpacity className="items-center justify-center space-y-2">
-      <View>MenuContainer</View>
+    <TouchableOpacity
+      className="items-center justify-center space-y-2"
+      onPress={handlePress}
+    >
+      <View
+        className={`w-24 h-24 p-2 shadow-sm rounded-full items-center justify-center ${
+          type === title.toLowerCase() ? "bg-gray-300" : ""
+        }`}
+      >
+        <Image source={imageSrc} className="w-full h-full object-contain" />
+      </View>
+      <Text className="text-[#00BCC9] text-[15px] font-semibold">{title}</Text>
     </TouchableOpacity>
   );
 };
