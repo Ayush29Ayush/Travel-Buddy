54f75d30043e0a1b121be5de2a49152e0b1ddbd0
Ayush Senapati <ayushsenapati123@gmail.com>
2023-02-17 15:15:19 +0530

    Item Card Container testing

--- components/ItemCardContainer.js
+++ components/ItemCardContainer.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
-const ItemCardContainer = () => {
+const ItemCardContainer = ({ imageSrc, title, location }) => {
   return (
-    <View>
-      <Text>ItemCarContainer</Text>
-    </View>
-  )
-}
+    <TouchableOpacity className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[160px] my-2">
+      <Image
+        source={{ uri: imageSrc }}
+        className="w-full h-40 rounded-md object-cover"
+      />
 
-export default ItemCardContainer
+      <Text className="text-[#428288] text-[18px] font-bold">
+        {title?.length > 14 ? `${title.slice(0, 14)}..` : title}
+      </Text>
+
+      <View className="flex-row items-center space-x-1">
+        <FontAwesome name="map-marker" size={20} color="#8597A2" />
+        <Text className="text-[#428288] text-[14px] font-bold">
+          {location?.length > 18 ? `${location.slice(0, 18)}..` : location}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ItemCardContainer;
