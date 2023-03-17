90d17b085e5d63e0ed522f7a8b369ed2feea8333
Ayush Senapati <ayushsenapati123@gmail.com>
2023-03-17 23:03:09 +0530

    changes done

--- components/ItemCardContainer.js
+++ components/ItemCardContainer.js
@@ -1,25 +1,37 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { FontAwesome } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-const ItemCardContainer = ({ imageSrc, title, location }) => {
+const ItemCardContainer = ({ imageSrc, title, location, data }) => {
+  const navigation = useNavigation();
+  
   return (
-    <TouchableOpacity className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[160px] my-2">
+    <TouchableOpacity
+      onPress={() => navigation.navigate("ItemScreen", { param: data })}
+      className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[182px] my-2"
+    >
       <Image
         source={{ uri: imageSrc }}
         className="w-full h-40 rounded-md object-cover"
       />
 
-      <Text className="text-[#428288] text-[18px] font-bold">
-        {title?.length > 14 ? `${title.slice(0, 14)}..` : title}
-      </Text>
+      {title ? (
+        <>
+          <Text className="text-[#428288] text-[18px] font-bold">
+            {title?.length > 14 ? `${title.slice(0, 14)}..` : title}
+          </Text>
 
-      <View className="flex-row items-center space-x-1">
-        <FontAwesome name="map-marker" size={20} color="#8597A2" />
-        <Text className="text-[#428288] text-[14px] font-bold">
-          {location?.length > 18 ? `${location.slice(0, 18)}..` : location}
-        </Text>
-      </View>
+          <View className="flex-row items-center space-x-1">
+            <FontAwesome name="map-marker" size={20} color="#8597A2" />
+            <Text className="text-[#428288] text-[14px] font-bold">
+              {location?.length > 18 ? `${title.slice(0, 18)}..` : location}
+            </Text>
+          </View>
+        </>
+      ) : (
+        <></>
+      )}
     </TouchableOpacity>
   );
 };
