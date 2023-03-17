a1e23a65f95083cafbe5bbd0dd023730603ea9ec
Ayush Senapati <ayushsenapati123@gmail.com>
2023-03-17 23:01:50 +0530

    index.js

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const getPlacesData = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: "25.15543993776612",
+          tr_latitude: "25.41257834546226",
+          bl_longitude: "51.39587210719369",
+          tr_longitude: "51.62812119686502",
+          limit: "30",
+          currency: "USD",
+          lunit: "km",
+          lang: "en_US",
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "c1941946ecmshd4859c6fa3d7d94p1b3ca4jsnd369b4805c81",
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
