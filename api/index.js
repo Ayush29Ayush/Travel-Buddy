0bdc2e182d4fb0a148df55aabdac370dade32496
Ayush Senapati <ayushsenapati123@gmail.com>
2023-03-17 23:57:49 +0530

    dynamic url

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-export const getPlacesData = async () => {
+export const getPlacesData = async (bl_lat, bl_lng, tr_lat, tr_lng, type) => {
   try {
     const {
       data: { data },
     } = await axios.get(
-      `https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary`,
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
       {
         params: {
-          bl_latitude: "25.15543993776612",
-          tr_latitude: "25.41257834546226",
-          bl_longitude: "51.39587210719369",
-          tr_longitude: "51.62812119686502",
+            bl_latitude: bl_lat ? bl_lat : "25.15543993776612",
+            tr_latitude: tr_lat ? tr_lat : "25.41257834546226",
+            bl_longitude: bl_lng ? bl_lng : "51.39587210719369",
+            tr_longitude: tr_lng ? tr_lng : "51.62812119686502",
           limit: "30",
           currency: "USD",
           lunit: "km",
